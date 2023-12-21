de39cb52b407ee4e3207e48fee683809e1eea009
Omer Polat <o.polat.tr@outlook.com>
2023-12-21 10:44:30 +0300

    Tasarımsal Düzenlemeler. Boostrap,jquery ve font-awesome kütüphanelerinin projeye eklenmesi

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -11,6 +11,10 @@ builder.Services.AddDbContext<RepositoryContext>(options =>
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.Run();
 
@@ -19,3 +23,4 @@ app.Run();
 // \
 // [ ]
 // |
+// ~
